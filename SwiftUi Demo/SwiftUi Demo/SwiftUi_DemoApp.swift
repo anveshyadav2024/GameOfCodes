dc809698115bd8593fdc2d93aa81e36f7304f529
anveshyadav2024 <167511938+anveshyadav2024@users.noreply.github.com>
2024-04-29 16:48:40 +0530

    Add files via upload

---
+++ SwiftUi Demo/SwiftUi Demo/SwiftUi_DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUi_DemoApp.swift
+//  SwiftUi Demo
+//
+//  Created by EL Group on 13/04/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUi_DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
