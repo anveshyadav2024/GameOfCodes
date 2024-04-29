5fcfe23c89ae0bb3103c2a8fbe656ff95ecfc1c7
anveshyadav2024 <167511938+anveshyadav2024@users.noreply.github.com>
2024-04-29 17:00:24 +0530

    Add files via upload
    Environment Object Demo

---
+++ EnvironmentObjectDemo/EnvironmentObjectDemo/EnvironmentObjectDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  EnvironmentObjectDemoApp.swift
+//  EnvironmentObjectDemo
+//
+//  Created by EL Group on 20/04/24.
+//
+
+import SwiftUI
+
+@main
+struct EnvironmentObjectDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
